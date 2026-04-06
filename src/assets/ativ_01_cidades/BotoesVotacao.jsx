7a467872bd85f926a2050003fe4fd809aaf4bcb3
letiarasil <leticiade.araujo@alu.ufc.br>
2026-04-06 15:22:06 -0300

    outro

---
+++ src/assets/ativ_01_cidades/BotoesVotacao.jsx
@@ -0,0 +1,11 @@
+function BotoesVotacao() {
+    return(
+        <div className="botoes">
+            <button onClick={votar}>Quixadá</button>
+            <button onClick={votar}>Iguatu</button>
+            <button onClick={votar}>Quixeramobim</button>
+            <button onClick={votar}>Banabuiú</button>
+      </div>
+    )
+}
+export default BotoesVotacao
